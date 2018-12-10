766a18a640228489b0418317b29a70554204a736
DESKTOP-EL58LL7\ixtla <tstecker@student.cscc.edu>
2018-12-10 16:17:31 -0500

    connected netlify

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Old Egg
+            Old Egg CI LOL!
           </a>
         </header>
       </div>
